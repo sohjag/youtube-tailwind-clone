d7dc50fabbbc29fda0b6c6ede8aea95df0a8daf6
Sohan <sohan.j12@gmail.com>
2023-10-04 16:15:54 +0530

    added more appbar icons

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -48,11 +48,48 @@ export function Appbar() {
         <Searchbar />
       </div>
       <div className="flex items-center sm:w-1/6 justify-end pr-5">
+        <div className="flex items-center pl-4">
+          <div className="p-2 rounded-full hover:bg-gray-800">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
+              />
+            </svg>
+          </div>
+        </div>
+
+        <div className="flex items-center pl-4 pr-4">
+          <div className="p-2 rounded-full hover:bg-gray-800">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5"
+              />
+            </svg>
+          </div>
+        </div>
+
         {isLoggedIn ? (
           <button onClick={handleSignIn}>Logged in</button>
         ) : (
           <button
-            className="p-2 flex justify-center border border-solid border-neutral-200 rounded-3xl pr-5 text-blue-300 font-semibold hover:bg-blue-300 hover:bg-opacity-50"
+            className="p-2 flex items-center border border-solid border-neutral-200 rounded-3xl pr-5 text-blue-300 font-semibold hover:bg-blue-300 hover:bg-opacity-50"
             onClick={handleSignIn}
           >
             Sign in
